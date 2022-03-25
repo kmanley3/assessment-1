53e886557c612aae425b54783224499a1d55b4eb
Kirsten Manley <kirstenmanley7@gmail.com>
2022-03-25 10:07:49 -0600

    First commit

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "This is the second step. Developers will look at the plans and determine what specifically needs to be built in order for success. Additionally, early ideas for prototypes are determined."
+const design = "Developers go into further detail and create the outline of how exactly the program will look and run. Often turned into a design document that other developers can look at and implement with code."
+const implementation = "Using the design document as reference, developers write code to make the design come to fruition."
+const testingAndIntegration = "Once all the code has been compiled, the program is given to testers who ensure not only that the new code works, but that when implemented with code from the program that wasn't changed, everything still works together. If it doesn't work, a bug report is made and developers fix it. Rinse and repeat until everything is (hopefully) working smoothly!"
+const maintain = "After a program/product has been released to the public, it is important to keep working on it. Maintenance can look like working on bugs that slipped through the cracks, working on new bugs that were never found, or improving the features."
